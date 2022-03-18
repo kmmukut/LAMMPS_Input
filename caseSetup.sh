1693a6270543fca92cccf8765263e4afd7a77227
Khaled Mosharraf Mukut <kmmukut@gmail.com>
2022-03-18 05:22:13 -0500

    clean up

--- caseSetup.sh
+++ caseSetup.sh
@@ -31,16 +31,15 @@ SEED=$(( $RANDOM % ($MAX + 1 - $MIN) + $MIN ))
 
 echo "Psudorandom seed= $SEED"
 
-sed -i ''  "s/NAME/$1/g" reaxc.control 
-sed -i ''  "s/NAME/$1/g" in.in 
-sed -i ''  "s/TEMP/$temp/g" in.in 
-sed -i ''  "s/DENS/$dens/g" in.in 
-sed -i ''  "s/EQ/$eqr/g" in.in 
-sed -i ''  "s/SEED/$SEED/g" in.in 
-sed -i ''  "s/STAT/$stat/g" in.in 
-
-sed -i ''  "s/NAME/$1/g" run.slrm_raj 
-sed -i ''  "s/NAME/$1/g" run.slrm_hpc
+sed -i   "s/NAME/$1/g" reaxc.control 
+sed -i   "s/NAME/$1/g" in.in 
+sed -i   "s/TEMP/$temp/g" in.in 
+sed -i   "s/DENS/$dens/g" in.in 
+sed -i   "s/EQ/$eqr/g" in.in 
+sed -i   "s/SEED/$SEED/g" in.in 
+sed -i   "s/STAT/$stat/g" in.in 
+sed -i   "s/NAME/$1/g" run.slrm_raj 
+sed -i   "s/NAME/$1/g" run.slrm_hpc
 
 
 echo "Set up is complete"
